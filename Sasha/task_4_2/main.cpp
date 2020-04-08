b9f85108c190cf0a5f9a39ef28b431001906d803
pringleskate <61543837+pringleskate@users.noreply.github.com>
2020-04-08 11:47:56 +0300

    Изменена задача 4_2

--- Sasha/task_4_2/main.cpp
+++ Sasha/task_4_2/main.cpp
@@ -2,30 +2,27 @@
 #include <assert.h>
 #include <stdio.h>
 
-/*
-                            Задача 4_2
+using namespace std;
 
+/* Дергачев Александр
+
+  Группа АПО-13
+
+ Условие задачи 4_2
+  
  Для сложения чисел используется старый компьютер. Время, затрачиваемое
  на нахождение суммы двух чисел равно их сумме. Таким образом для нахождения
  суммы чисел 1,2,3 может потребоваться разное время, в зависимости от порядка
  вычислений.
- ((1+2)+3) -> 1+2 + 3+3 = 9
- ((1+3)+2) -> 1+3 + 4+2 = 10
- ((2+3)+1) -> 2+3 + 5+1 = 11
- Требуется написать программу, которая определяет минимальное время, достаточное
- для вычисления суммы заданного набора чисел.
- Требуемое время работы O(n*log(n)).
 */
 
-// Куча целых чисел, реализованная с помощью массива
 class class_Array
 {
 private:
     int *array;	                // Массив данных
     int size_array;	            // Размер кучи
     int elements_count;	        // Количество уже заполненных элементов
-    void swap(int, int);		// Смена мест элементов с заданными индексами
-
+  
 public:
     class_Array(int size);
     ~class_Array() { delete array; };
@@ -40,17 +37,19 @@ public:
 
 int class_Array::getFirst()
 {
+  //извлекаем первый элемент, при этом помещаем на его место последний элемент в куче, а также балансируем кучу
     int tmp = array[0];
-    swap(0, --elements_count);
+    swap(array[0], array[elements_count]);
     moveDown(0);
     return tmp;
 }
 
+//балансируем кучу путем спуска элемента вниз 
 void class_Array::moveDown(int i)
 {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    // Ищем меньшего сына, если такой есть.
+    // Ищем меньшего сына
     int largest = i;
 
     if(left < elements_count && array[left] < array[i])
@@ -59,14 +58,17 @@ void class_Array::moveDown(int i)
     if(right < elements_count && array[right] < array[largest])
         largest = right;
 
-    // Если меньший сын есть, то проталкиваем корень в него.
+    // Если меньший сын есть, то проталкиваем его в корень кучи 
     if(largest != i)
     {
-        swap(i, largest);
+        swap(array[i], array[largest]);
         this->moveDown(largest);
     }
 }
 
+
+
+//балансируем кучу путем поднятия элемента 
 void class_Array::moveUp( int i )
 {
     int parent;
@@ -76,17 +78,10 @@ void class_Array::moveUp( int i )
         parent = (i - 1) / 2;
 
         if(array[i] <= array[parent])
-            swap(i, parent);
+            swap(array[i], array[parent]);
 
         i = parent;
     }
-}
-
-void class_Array::swap(int i, int j)
-{
-    array[i] = array[i] ^ array[j];
-    array[j] = array[i] ^ array[j];
-    array[i] = array[i] ^ array[j];
 }
 
 class_Array::class_Array(int size): size_array(size), elements_count(0)
@@ -96,8 +91,8 @@ class_Array::class_Array(int size): size_array(size), elements_count(0)
 
 void class_Array::addElement(int Data)
 {
-    array[elements_count] = Data;
-    moveUp(elements_count);
+    array[elements_count] = Data; //добавляем новый элемент
+    moveUp(elements_count);       //балансируем кучу
     elements_count++;
 }
 
@@ -120,7 +115,7 @@ int calculateTime(class_Array *Intarray)
 
 int main()
 {
-    //Получение количство слогаемых
+    //Получение количества слагаемых
     int elements_count = 0;
 
     if( scanf("%d\n", &elements_count) != 1)
